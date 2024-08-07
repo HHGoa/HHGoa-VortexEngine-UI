0667bdb7d5783d6bd6ae2d90d2c45cd5b548f8a2
vishalmet <vishalmet003@gmail.com>
2024-08-07 19:05:46 +0530

    dashboard

--- frontend/pages/Dashboard.tsx
+++ frontend/pages/Dashboard.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import GraphFetch, { DataAdded } from '../components/GraphFetch';
+import React, { useState } from "react";
+import GraphFetch, { DataAdded } from "../components/GraphFetch";
+import GradientBackground from "@/components/Background";
+import Navbar2 from "@/components/Navbar2";
 
 const Dashboard: React.FC = () => {
   const [dataAddeds, setDataAddeds] = useState<DataAdded[]>([]);
@@ -9,37 +11,41 @@ const Dashboard: React.FC = () => {
   };
 
   return (
-    <div className='pt-20 px-4'>
-      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-      <GraphFetch onDataReceived={handleDataReceived} />
-      <div className="overflow-x-auto">
-        {dataAddeds.length > 0 ? (
-          <table className="min-w-full bg-black border border-gray-200">
-            <thead>
-              <tr>
-                <th className="py-2 px-4 border-b">ID</th>
-                <th className="py-2 px-4 border-b">VortexScan ID</th>
-                <th className="py-2 px-4 border-b">User Address</th>
-                <th className="py-2 px-4 border-b">Data URI</th>
-              </tr>
-            </thead>
-            <tbody>
-              {dataAddeds.map((item) => (
-                <tr key={item.id}>
-                  <td className="py-2 px-4 border-b">{item.id}</td>
-                  <td className="py-2 px-4 border-b">{item.VortexScan_id}</td>
-                  <td className="py-2 px-4 border-b">{item.user}</td>
-                  <td className="py-2 px-4 border-b">{item.dataUri}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <p className="text-center mt-4">No data available</p>
-        )}
-      </div>
+    <div className=" ">
+      <GradientBackground>
+        <Navbar2 />
+        <div className="pt-24 md:pt-36 min-h-screen px-4">
+          <GraphFetch onDataReceived={handleDataReceived} />
+          <div className="overflow-x-auto">
+            {dataAddeds.length > 0 ? (
+              <table className="mx-auto bg-transparent border border-gray-200">
+                <thead>
+                  <tr>
+                    <th className="py-2 px-4 brandy-font border border-gray-200">ID</th>
+                    <th className="py-2 px-4 brandy-font border border-gray-200">VortexScan ID</th>
+                    <th className="py-2 px-4 brandy-font border border-gray-200">User Address</th>
+                    <th className="py-2 px-4 brandy-font border border-gray-200">Data URI</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {dataAddeds.map((item) => (
+                    <tr key={item.id}>
+                      <td className="py-2 px-4 border border-gray-200">{item.id}</td>
+                      <td className="py-2 px-4 text-center border border-gray-200">{item.VortexScan_id}</td>
+                      <td className="py-2 px-4 border border-gray-200">{item.user}</td>
+                      <td className="py-2 px-4 border border-gray-200">{item.dataUri}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <p className="text-center mt-4">No data available</p>
+            )}
+          </div>
+        </div>
+      </GradientBackground>
     </div>
-  );  
+  );
 };
 
 export default Dashboard;
